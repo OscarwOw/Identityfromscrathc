1b9f0e4dd9016429f295e270385b41c8e61ba1e7
OscarwOw <OscarwOw210@gmail.com>
2022-02-05 11:23:25 +0100

    Add project files.

---
+++ Identityfromscrathc/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace Identityfromscrathc.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential credential { get; set; }
+        public void OnGet()
+        {
+            
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            if(credential.Username == "admin" && credential.Password == "password")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@mail.com")
+
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+
+                return RedirectToPage("/Index");
+            }
+            return Page();
+        }
+
+
+        public class Credential
+        {
+            [Required]
+            [Display(Name ="User Name")]
+            public string Username { get; set; }
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+
+
+    }
+}
